8907fe17bb55547b02c8eddd2465d82c4565e5ec
bkdwei <bkdwei@163.com>
2019-03-23 16:40:25 +0800

    修改快捷方式的创建

--- install_script.py
+++ install_script.py
@@ -1,16 +1,29 @@
 import sys
 import os
-import shutil
 from setuptools.command.install import install
+
+project_name = "kdLaunchPad"
 class install_cmd(install):
     def run(self):
         install.run(self)
-        if sys.platform =="win32" :
-                path =  os.path.join(os.path.expanduser("~"), 'Desktop','kdLaunchPad.bat')
-                print("copy windows's script in " + path)
-                shutil.copyfile("script/kdLaunchPad.bat",path)
-        elif sys.platform == "linux":
-                path = '/usr/share/applications/kdLaunchPad.desktop'
-                print("copy linux's desktop file in " + path)
-                shutil.copyfile("script/kdLaunchPad.desktop",path)
-               
+        try :
+            if sys.platform =="win32" :
+                    script_file =  os.path.join(self._get_desktop_folder(),'kdLaunchPad.bat')
+                    with open(script_file, "w") as f:
+                        f.write("@echo off\r\nstart " + project_name)
+            elif sys.platform == "linux":
+                    import stat
+                    script_file =  os.path.join(self._get_desktop_folder(),'kdLaunchPad.sh')
+                    with open(script_file, "w") as f:
+                        f.write("#!/bin/sh\n" + project_name)
+                        st = os.stat(script_file)
+                        os.chmod(script_file, st.st_mode | stat.S_IEXEC)
+        except Exception as e:
+            print("can not create start script." + str(e))
+    def _get_desktop_folder(self):
+        import subprocess
+        try:
+            return subprocess.check_output(['xdg-user-dir',
+                                            'DESKTOP']).decode('utf-8').strip()
+        except Exception:
+            return os.path.join(os.path.expanduser('~'), 'Desktop')   
--- script/kdLaunchPad.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start kdLaunchPad.exe
